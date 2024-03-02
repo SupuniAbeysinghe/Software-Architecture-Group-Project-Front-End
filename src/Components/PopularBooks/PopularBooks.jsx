ed9809b9c37521c3c063c2c0c3c6552d64424394
uviniranasinghe <139993670+uviniranasinghe@users.noreply.github.com>
2024-03-02 17:41:06 +0530

    popular books 3

--- src/Components/PopularBooks/PopularBooks.jsx
+++ src/Components/PopularBooks/PopularBooks.jsx
@@ -2,7 +2,13 @@ import './PopularBooks.css'
 
 import TitleTypeOne from '../../UI/TitleTypeOne/TitleTypeOne'
 
-export default function Popularbooks(){
+import { galleryData } from '../../Data/Data'
+
+export default function Popularbooks(){    
+        const filterItems = activeButton === 'all'? galleryData:galleryData.
+        filter((item)=>item.category === activeButton);
+    
+
     return(
         <section>
         <div className="container popularbooks-container">
@@ -28,6 +34,22 @@ export default function Popularbooks(){
                     Fictional
                 </button>
 
+            </div>
+            <div className="gallery">
+                {filterItems.map(({name,writer,price,image},index)=>{
+                    return(
+                        <div className="gallery-item" key={index}>
+                            <div className="popularbook-image">
+                                <img src={image} alt="" />
+                            </div>
+                            <div className='popularbook-info'>
+                                <h4>{name}</h4>
+                                <div><small>{writer}</small></div>
+                                <h5><span>{price}</span></h5>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
 
 
