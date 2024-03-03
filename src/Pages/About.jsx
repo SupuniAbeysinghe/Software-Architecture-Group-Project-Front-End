8ce434e79a6d9c032185e11bfe401a7ac3604041
uviniranasinghe <139993670+uviniranasinghe@users.noreply.github.com>
2024-03-03 14:38:10 +0530

    front end 5

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,5 @@
+import './About.css'
+import Aboutimg from '../assets/about.jpg';
 
 export default function About(){
     return (
@@ -5,13 +7,11 @@ export default function About(){
         
 
         <p><br /><h3>
-            <img src="" alt="" />Welcome to The Bookshelf, your online destination for all things literary! <br /></h3><br />
-At The Bookshelf, we believe that books have the power to inspire, educate, and transform lives. Whether you're a seasoned reader looking for your next great adventure or a newcomer eager to explore the world of literature, we have something for everyone.
-Our carefully curated collection features a wide range of genres, from classic literature to contemporary fiction, from thought-provoking non-fiction to captivating children's books. We strive to offer a diverse selection that reflects the rich tapestry of human experiences and perspectives.
-But The Bookshelf is more than just a bookstore. It's a community of book lovers, united by our passion for reading. Join us for author interviews, book clubs, and literary events that celebrate the written word. Connect with fellow readers, share your favorite books, and discover new literary treasures together.
-At The Bookshelf, we're committed to providing a seamless shopping experience. Our user-friendly website makes it easy to browse, purchase, and track your orders. We offer fast and reliable shipping, so you can start enjoying your new books as soon as possible.
-
-Thank you for choosing The Bookshelf as your literary companion. We look forward to helping you discover your next great read!</p>
+        
+                    <img src={Aboutimg} alt="about" className='aboutimg' />
+                Welcome to The Bookshelf, your online destination for all things literary! <br /></h3><br />
+                At The Bookshelf, we believe in the transformative power of books. Whether you're a seasoned reader or new to literature, our curated collection has something for everyone. From classic to contemporary, fiction to non-fiction, and children's books to thought-provoking reads, we offer a diverse selection reflecting human experiences. More than a bookstore, we're a community of book lovers celebrating the written word through author interviews, book clubs, and literary events. Join us to connect with fellow readers, share favorites, and discover new treasures. Our user-friendly website ensures a seamless shopping experience with fast, reliable shipping. 
+                <br />Thank you for choosing The Bookshelf as your literary companion; we're excited to help you find your next great read!</p>
         </>
     )
 }
